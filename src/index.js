a94d14b3b85038361e4ddd80003a333a38150003
Ryan Noort <ryan.noort@zayzoon.com>
2021-06-20 11:55:19 -0600

    add foreground blur effect for later

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Frames from './Frames';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="absolute h-screen w-screen bg-white z-50 opacity-90">
+    </div>
     <div className="h-screen">
       <Frames />
      </div>
